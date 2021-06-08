7ad03d86c036317a08ad98760c4d1716d663c3bd
Fran Diéguez <fran.dieguez@mabishu.com>
2021-06-08 08:22:44 +0200

    Update BookNowButton.tsx

--- components/Buttons/BookNowButton.tsx
+++ components/Buttons/BookNowButton.tsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export default function BookNowButton() {
   return (
-    <button className="btn button book_now">
-      <a
-        href="https://www.booking.com/hotel/es/albergue-celenis.html"
-        title="Book some room"
-        className="book_now_content"
-      >
-        <img src="./icons/icon-house.svg" alt="" />
-        Book now
-      </a>
-    </button>
+    <a  className="btn button book_now"
+      href="https://www.booking.com/hotel/es/albergue-celenis.html"
+      title="Book some room"
+      className="book_now_content"
+    >
+      <img src="./icons/icon-house.svg" alt="" />
+      Book now
+    </a>
   );
 }
