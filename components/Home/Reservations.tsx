7db8d01ef066a9fd87429aa58be916e9524a5d87
Fran Diéguez <fran.dieguez@mabishu.com>
2021-06-21 22:32:14 +0200

    Remove book widget

--- components/Home/Reservations.tsx
+++ components/Home/Reservations.tsx
@@ -3,12 +3,6 @@ import React from "react";
 export default function Reservations() {
   return (
     <>
-      <div
-        className="ibe page-wrapper"
-        data-region="emea"
-        data-channelcode="alberguecelenisdirect"
-        data-widget="embed"
-      ></div>
     </>
   );
 }
