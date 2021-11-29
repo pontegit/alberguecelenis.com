b5d0b6db0e23246fdca0058a080e793c3c66232e
pontegit <77108204+pontegit@users.noreply.github.com>
2021-11-29 11:51:02 +0100

    Update index.tsx

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
                 Facebook
               </a>
             </li>  
-            <li>
+            {/* <li>
               <a
                 href="https://twitter.com/alberguecelenis"
                 title="Visit our Twitter profile"
@@ -60,7 +60,7 @@ const Footer = () => {
                 </div>
                 Twitter
               </a>
-            </li>
+            </li> */}
             <li>
               <a
                 href="https://www.instagram.com/alberguecelenis"
