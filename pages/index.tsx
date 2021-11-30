d0e75546ba8b76023c3907940d3d1ee8e1603759
pontegit <77108204+pontegit@users.noreply.github.com>
2021-11-30 11:20:20 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,31 +12,6 @@ import ContactUs from "../components/Home/ContactUs";
 import Rooms from "../components/Home/Rooms";
 import Reservations from "../components/Home/Reservations";
 
-import Script from 'next/script';
-
-const Home = () => {
-  return (
-    <div class="container">
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-213807336-1');
-        `}
-      </Script>
-    </div>
-  )
-};
-
-export default Home;
-
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Albergue Celenis | Descanso en Caldas de Reis del Camino de Santiago">
