b9f93ed4ff4796c5626c687fbe27c4ad1e6c2d6a
pontegit <77108204+pontegit@users.noreply.github.com>
2021-11-30 11:40:06 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -22,17 +22,7 @@ const Layout: React.FunctionComponent<Props> = ({
         content="Un lugar idílico donde descansar en el Camino Portugés de Santiago en Caldas de Reis."
       />      
 </script>
-    <!-- Google Analytics -->
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-XXXXX-Y', 'auto');
-ga('send', 'pageview');
-</script>
-<!-- End Google Analytics -->
+   
     </Head>
     {children}
     <script
