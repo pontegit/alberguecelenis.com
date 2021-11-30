297137cc78bd4df34c8783b0907c47d0e61eef5d
pontegit <77108204+pontegit@users.noreply.github.com>
2021-11-30 11:17:43 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import Script from 'next/script';
-
-const Home = () => {
-  return (
-    <div class="container">
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-213807336-1');
-        `}
-      </Script>
-    </div>
-  )
-}
-
-export default Home;
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
