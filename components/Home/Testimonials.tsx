2ed92a817726c1b3008d8f6d12842e8b9f94b87e
pontegit <77108204+pontegit@users.noreply.github.com>
2021-12-01 11:40:51 +0100

    Update Testimonials.tsx

--- components/Home/Testimonials.tsx
+++ components/Home/Testimonials.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Testimonials() {
   return (
-    <div id="testimonials" className="testimonials">
+    <div id="testimonials" className="testimonials t2">
       <div className="page-wrapper">
         <h3 className="testimonials_title">Testimonials</h3>
         <div className="testimonials_list">
