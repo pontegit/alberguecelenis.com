942be4e915dcf3d60aa5920f11c13e54472e1a48
pontegit <77108204+pontegit@users.noreply.github.com>
2022-01-27 09:20:04 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -20,9 +20,16 @@ const Layout: React.FunctionComponent<Props> = ({
       <meta
         name="description"
         content="Un lugar idílico donde descansar en el Camino Portugés de Santiago en Caldas de Reis."
-      />      
+      />
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NS3DMPV');</script>
+<!-- End Google Tag Manager -->
 </script>
-   <!-- Global site tag (gtag.js) - Google Analytics -->
+   <!-- Global site tag (gtag.js) - Google Analytics
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-213807336-1%22%3E</script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -30,7 +37,18 @@ const Layout: React.FunctionComponent<Props> = ({
   gtag('js', new Date());
 
   gtag('config', 'UA-213807336-1');
-</script>
+</script> -->
+                   
+<!-- Global site tag (gtag.js) - Google Ads: 10816458096
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10816458096"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-10816458096');
+</script> -->
+                   
     </Head>
     {children}
     <script
